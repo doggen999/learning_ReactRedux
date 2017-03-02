2a3f517f53b0508864d96e090559616f8a702c2a
Natan Williams <natan.williams@tretton37.com>
2017-03-02 12:17:09 +0100

    Basic Walking Skeleton

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import BeerRatingList from '../containers/beerRatingsList_container'
+
 class App extends Component {
   render() {
     return(
       <div>
+        <BeerRatingList/>
       </div>
     )
   }
